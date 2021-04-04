8b438946d715ca4947e96465ebf8ffc48899ad19
Pedrucho1213 <pmpedrotorres@gmail.com>
2021-04-03 18:44:10 -0600

    Proyecto IONIC 5 MarvelApi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,18 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  {
+    path: 'comics',
+    loadChildren: () => import('./comics/comics.module').then( m => m.ComicsPageModule)
+  },
+  {
+    path: 'series',
+    loadChildren: () => import('./series/series.module').then( m => m.SeriesPageModule)
+  },
+  {
+    path: 'personajes',
+    loadChildren: () => import('./personajes/personajes.module').then( m => m.PersonajesPageModule)
   }
 ];
 
