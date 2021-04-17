26c9e1341202fde2f145cda9eb99e1cf82522812
Pedrucho1213 <pmpedrotorres@gmail.com>
2021-04-17 13:27:41 -0500

    Agregando funciones de la api marvel

--- src/app/services/api-json.service.ts
+++ src/app/services/api-json.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,8 @@ export class ApiJsonService {
   private publiKey = '3519d8ca22dc39287f25ec9fe72c261f';
   private baseComics = 'https://gateway.marvel.com:443/v1/public/comics?orderBy=issueNumber&limit=10&apikey=' + this.publiKey;
 
+  private baseSerie = 'https://gateway.marvel.com:443/v1/public/series?limit=25&apikey=' + this.publiKey;
+
   constructor(
     private readonly http: HttpClient
   ) {
@@ -17,6 +19,10 @@ export class ApiJsonService {
 
   getComics(): Observable<any> {
     return this.http.get<any>(this.baseComics).pipe(map((data: any) => data.data.results));
+  }
+
+  getSeries(): Observable<any> {
+    return this.http.get<any>(this.baseSerie).pipe(map((data: any) => data.data.results));
   }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ApiJsonService} from '../services/api-json.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
+  templateUrl: 'tab2.pacge.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  serie: Observable<any>;
 
-  constructor() {}
+  constructor(
+    private api: ApiJsonService
+  ) {
+  }
 
+  ngOnInit() {
+    this.getSerie();
+  }
+
+  getSerie() {
+    this.serie = this.api.getSeries();
+  }
 }
